5db2a3bbb629efb7b748e43893ad1f494de4a666
BRQ\alessandrolima <alessandrolima@brq.com>
2019-01-09 14:39:10 -0200

    Recycler View Parte II

--- app/src/main/java/br/com/alessandro/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alessandro/ceep/ui/activity/FormularioNotaActivity.java
@@ -1,24 +1,53 @@
 package br.com.alessandro.ceep.ui.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.widget.TextView;
 import br.com.alessandro.ceep.R;
 import br.com.alessandro.ceep.model.Nota;
 
-import static br.com.alessandro.ceep.ui.activity.NotaActivityConstantes.CHAVE_NOTA;
-import static br.com.alessandro.ceep.ui.activity.NotaActivityConstantes.CODIGO_RESULTADO_NOTA_CRIADA;
+import static br.com.alessandro.ceep.ui.activity.NotaActivityConstantes.*;
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
+
+    public static final String TITULO_APPBAR_INSERE = "Insere nota";
+    private static final String TITULO_APPBAR_ALTERA = "Altera nota";
+    private int posicaoRecebida = POSICAO_INVALIDA;
+    private TextView titulo;
+    private TextView descricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
+
+        setTitle(TITULO_APPBAR_INSERE);
+
+        iniciaizaCampos();
+
+        Intent dadosRecebidos = getIntent();
+        if (dadosRecebidos.hasExtra(CHAVE_NOTA)) {
+            setTitle(TITULO_APPBAR_ALTERA);
+            Nota notaRecebida = (Nota) dadosRecebidos.getSerializableExtra(CHAVE_NOTA);
+            posicaoRecebida = dadosRecebidos.getIntExtra(CHAVE_POSICAO, POSICAO_INVALIDA);
+
+            preencheCampos(notaRecebida);
+        }
+    }
+
+    private void preencheCampos(Nota notaRecebida) {
+        titulo.setText(notaRecebida.getTitulo());
+        descricao.setText(notaRecebida.getDescricao());
+    }
+
+    private void iniciaizaCampos() {
+        titulo = findViewById(R.id.formulario_nota_titulo);
+        descricao = findViewById(R.id.formulario_nota_descricao);
     }
 
     @Override
@@ -29,7 +58,7 @@ public class FormularioNotaActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(ehMenuSalvaNota(item)){
+        if (ehMenuSalvaNota(item)) {
             Nota notaCriada = criaNota();
             retornaNota(notaCriada);
             finish();
@@ -40,13 +69,14 @@ public class FormularioNotaActivity extends AppCompatActivity {
     private void retornaNota(Nota nota) {
         Intent resultadoInsercao = new Intent();
         resultadoInsercao.putExtra(CHAVE_NOTA, nota);
-        setResult(CODIGO_RESULTADO_NOTA_CRIADA,resultadoInsercao);
+        resultadoInsercao.putExtra(CHAVE_POSICAO, posicaoRecebida);
+        setResult(Activity.RESULT_OK, resultadoInsercao);
     }
 
 
     private Nota criaNota() {
-        EditText titulo = findViewById(R.id.formulario_nota_titulo);
-        EditText descricao = findViewById(R.id.formulario_nota_descricao);
+        titulo = findViewById(R.id.formulario_nota_titulo);
+        descricao = findViewById(R.id.formulario_nota_descricao);
         return new Nota(titulo.getText().toString(),
                 descricao.getText().toString());
     }
---
+++ app/src/main/java/br/com/alessandro/ceep/ui/recyclerview/adapter/listener/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package br.com.alessandro.ceep.ui.recyclerview.adapter.listener;
+
+import br.com.alessandro.ceep.model.Nota;
+
+public interface OnItemClickListener {
+
+    void onItemClick(Nota nota, int posicao);
+}
